27ca17119b83dcbaef11f441412d04a13568bc81
Swallow8801 <bipa30@naver.com>
2025-03-04 16:49:25 +0900

    N과 M(2) / 실버3

--- jaeyub/Week 1/15650.py
+++ jaeyub/Week 1/15650.py
@@ -1,6 +1,15 @@
-import itertools
+N,M = map(int,input().split())
+조합 = []
 
-x,y = map(int,input().split())
+def dfs():
+    if len(조합) == M:
+        print(" ".join(map(str, 조합)))
+        return
+    for i in range(1,N+1):
+        if i not in 조합:
+            if len(조합) == 0 or 조합[-1] < i :
+                조합.append(i)
+                dfs()
+                조합.pop()
 
-for z in itertools.combinations(list(range(1,x+1)),y):
-    print(" ".join(map(str,z)))
+dfs()
