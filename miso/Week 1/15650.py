21250aa7f6cd50fe8122aafeefb00de28d25598d
Myusol <qoralth25@gmail.com>
2025-03-04 18:21:32 +0900

    N과 M (2) / 실버3

--- miso/Week 1/15650.py
+++ miso/Week 1/15650.py
@@ -10,7 +10,7 @@ def nmasc():
 		print(' '.join(map(str,l)))
 		return
 	for i in range(1, n+1):
-		if i not in l and (not l or l[-1] < i):
+		if not l or l[-1] < i:
 			l.append(i)
 			nmasc()
 			l.pop()
