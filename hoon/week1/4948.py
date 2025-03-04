932fa06cc13be6f56b1007666600061d8595f402
SeongHoon Lee <150895257+seonghoonL@users.noreply.github.com>
2025-03-05 00:39:42 +0900

    베르트랑 공준 / 실버2 / 00:55
    아오

---
+++ hoon/week1/4948.py
@@ -0,0 +1,20 @@
+while True:
+    n = int(input()) # 10
+    if n == 0:
+        exit()
+
+    m = n * 2
+    count = 0
+    for i in range(n+1, m+1):
+        if n <= 1:
+            count += 1
+            break
+        for j in range(2,int(i**0.5)+1):
+            if i % j == 0:
+                break
+        else:
+            count += 1
+
+    print(count)
+
+#시간초과..
