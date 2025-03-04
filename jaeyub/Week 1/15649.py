b2ed51414d25daf02808a5b76ad1b2eb36b82593
Swallow8801 <bipa30@naver.com>
2025-03-04 16:50:18 +0900

    N과 M(1) / 실버3

--- jaeyub/Week 1/15649.py
+++ jaeyub/Week 1/15649.py
@@ -1,15 +1,14 @@
 N,M = map(int,input().split())
-조합 = []
+순열 = []
 
 def dfs():
-    if len(조합) == M:
-        print(" ".join(map(str, 조합)))
+    if len(순열) == M:
+        print(" ".join(map(str, 순열)))
         return
     for i in range(1,N+1):
-        if i not in 조합:
-            if len(조합) == 0 or 조합[-1] < i :
-                조합.append(i)
-                dfs()
-                조합.pop()
+        if i not in 순열:
+            순열.append(i)
+            dfs()
+            순열.pop()
 
 dfs()
