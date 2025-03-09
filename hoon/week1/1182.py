ff3c646ae5f55721b0d5b0a161548d67945d8b8b
SeongHoon Lee <150895257+seonghoonL@users.noreply.github.com>
2025-03-09 21:46:44 +0900

    부분수열의 합 / 실버2

--- hoon/week1/1182.py
+++ hoon/week1/1182.py
@@ -2,11 +2,19 @@ N, S = map(int, input().split()) # 5, 0
 numbers = list(map(int, input().split())) # -7, -3, -2, 5, 8
 count = 0
 
-for k in range(1,k-1,1):
-    for i in range(2, N, 1): # 묶는 숫자 개수
-        for j in range(0, N-1, 1): # 인덱스번호
-            sumarr = sum(numbers[j]+numbers[j+1])
-            if sumarr == S:
-                count += 1
+# for k in range(1,N-1,1):
+#     for i in range(2, N, 1): # 묶는 숫자 개수
+#         for j in range(0, N-1, 1): # 인덱스번호
+#             sumarr = sum(numbers[j]+numbers[j+1])
+#             if sumarr == S:
+#                 count += 1
 
-print (count)
+# print (count)
+
+def dfs(idx, total, cnt):
+    if idx == N:
+        return 1 if cnt > 0 and total == S else 0
+    return dfs(idx + 1, total + numbers[idx], cnt + 1) + dfs(idx + 1, total, cnt)
+
+count = dfs(0, 0, 0)
+print(count)
