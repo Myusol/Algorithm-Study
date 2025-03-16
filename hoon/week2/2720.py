6d05ee1a3cb2193e37b216a17e0c78695eb26bba
SeongHoon Lee <150895257+seonghoonL@users.noreply.github.com>
2025-03-16 20:44:39 +0900

    세탁소 사장 / 브론즈3

--- hoon/week2/2720.py
+++ hoon/week2/2720.py
@@ -3,12 +3,12 @@ for _ in range(T):
     c = int(input())
     # 쿼터(25센트)
     q = c // 25
-    rem = c % 25
+    r = c % 25
     # 다임(10센트)
-    d = rem // 10
-    rem = rem % 10
+    d = r // 10
+    r = r % 10
     # 니켈(5센트)
-    n = rem // 5
+    n = r // 5
     # 페니(1센트)
-    p = rem % 5
+    p = r % 5
     print(q, d, n, p)
