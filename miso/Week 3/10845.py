911828c7e4f6471e828e09b94bbe972ac82d9db7
Myusol <qoralth25@gmail.com>
2025-03-17 16:17:34 +0900

    큐 / 실버4

---
+++ miso/Week 3/10845.py
@@ -0,0 +1,28 @@
+import sys
+queue=[]
+for _ in range(int(input())):
+    command=sys.stdin.readline().split()
+    if command[0]=='push':
+        queue.append(command[1])
+    elif command[0]=='pop':
+        if queue:
+            print(queue.pop(0))
+        else:
+            print(-1)
+    elif command[0]=='size':
+        print(len(queue))
+    elif command[0]=='empty':
+        if not queue:
+            print(1)
+        else:
+            print(0)
+    elif command[0]=='front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif command[0]=='back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
