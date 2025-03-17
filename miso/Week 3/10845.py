3b9b606ba311cdd4e4b945938c3ef713b4e14140
Myusol <qoralth25@gmail.com>
2025-03-17 16:21:45 +0900

    큐 / 실버4

--- miso/Week 3/10845.py
+++ miso/Week 3/10845.py
@@ -5,24 +5,12 @@ for _ in range(int(input())):
     if command[0]=='push':
         queue.append(command[1])
     elif command[0]=='pop':
-        if queue:
-            print(queue.pop(0))
-        else:
-            print(-1)
+        print(queue.pop(0) if queue else -1)
     elif command[0]=='size':
         print(len(queue))
     elif command[0]=='empty':
-        if not queue:
-            print(1)
-        else:
-            print(0)
+        print(1 if not queue else 0)
     elif command[0]=='front':
-        if queue:
-            print(queue[0])
-        else:
-            print(-1)
+        print(queue[0] if queue else -1)
     elif command[0]=='back':
-        if queue:
-            print(queue[-1])
-        else:
-            print(-1)
+        print(queue[-1] if queue else -1)
