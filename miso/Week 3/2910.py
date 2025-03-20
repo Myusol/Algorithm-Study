02bf2e4efde0ea0a014bc9ac57171cbc778820a1
Myusol <qoralth25@gmail.com>
2025-03-20 18:38:00 +0900

    빈도 정렬 / 실버3

--- miso/Week 3/2910.py
+++ miso/Week 3/2910.py
@@ -18,3 +18,13 @@ for i in arr:
     dict[i]=dict.get(i,0)+1
 sort_dict=sorted(dict.items(),key=lambda x:x[1],reverse=True)
 print(*[(str(i[0])+' ')*i[1] for i in sort_dict],sep='')
+
+#######
+
+import sys
+from collections import Counter
+n,c=map(int,sys.stdin.readline().split())
+arr=list(map(int,sys.stdin.readline().split()))
+for key,value in Counter(arr).most_common():
+    for _ in range(value):
+        print(key, end=' ')
