54f405fd255b3b14d299b47fdac7f69205a4eadc
SeongHoon Lee <150895257+seonghoonL@users.noreply.github.com>
2025-03-21 03:03:42 +0900

    최소 힙 / 실버2
    import sys
    input = sys.stdin.readline
    위 내용을 앞으로는 꼭 추가하자..

--- hoon/week3/1927.py
+++ hoon/week3/1927.py
@@ -1,14 +1,15 @@
 # 최소 힙 / 실버2
+import sys
+import heapq
+input = sys.stdin.readline
 n = int(input())
-lis = [] # 정수 리스트
+heap = []
 for _ in range(n):
     a = int(input())
     if a > 0:
-        lis.append(a)
-    elif a ==0:
-        if len(lis) == 0:
-            print(0)
+        heapq.heappush(heap, a)
+    else:
+        if heap:
+            print(heapq.heappop(heap))
         else:
-            minv = min(lis)
-            print(minv)
-            lis.remove(minv)
+            print(0)
