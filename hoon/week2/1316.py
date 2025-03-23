a18585bfa8caaa921469a08df5f4dae77082df80
SeongHoon Lee <150895257+seonghoonL@users.noreply.github.com>
2025-03-23 16:36:19 +0900

    그룹 단어 체커 / 실버5

--- hoon/week2/1316.py
+++ hoon/week2/1316.py
@@ -2,8 +2,14 @@
 T = int(input())
 count = 0
 for _ in range(T):
-    s = input()
-    c=''
+    s = input().strip()
+    c = ''
+    group = True
     for i in range(len(s)):
         if s[i] != c and s[i] in s[:i]:
-            
+            group = False
+            break
+        c = s[i]
+    if group:
+        count += 1
+print(count)
