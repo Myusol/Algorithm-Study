46cbc856b1b42d835eb6779117e6b63060fac8fe
Swallow8801 <bipa30@naver.com>
2025-05-18 15:48:13 +0900

    감시 / 골드3

--- jaeyub/Week 11/15683.py
+++ jaeyub/Week 11/15683.py
@@ -1,54 +1,53 @@
-from collections import deque
-from itertools import combinations
+import copy
 
-def bfs(vgraph,vrs):
-    copy_vrs = deque(vrs)
-    visited = [[False]*M for _ in range(N)]
-    dx = [-1,0,1,0]
-    dy = [0,1,0,-1]
-    for x,y in copy_vrs:
-        visited[x][y] = True
+N, M = map(int, input().split())
+maps = [[int(x) for x in input().split()] for _ in range(N)]
 
-    while copy_vrs:
-        x,y = copy_vrs.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<N and 0<=ny<M:
-                if visited[nx][ny] == False and vgraph[nx][ny] == 0:
-                    vgraph[nx][ny] = 2
-                    visited[nx][ny] = True
-                    copy_vrs.append((nx,ny))
-    return vgraph
+dv = [(-1, 0), (0,1), (1, 0), (0, -1)]
 
-N,M = map(int,input().split())
-graph = []
-empty = []
-virus = deque()
+cctv_directions = {1: [[0], [1], [2], [3]],
+                   2: [[1, 3], [0, 2]],
+                   3: [[0, 1], [1, 2], [2, 3], [3, 0]],
+                   4: [[0, 1, 2], [0, 1, 3], [1, 2, 3], [0, 2, 3]],
+                   5: [[0, 1, 2, 3]]}
+
+min_value = 1e9
+
+cctv = []
 for i in range(N):
-    arr = list(map(int, input().split()))
     for j in range(M):
-        if arr[j] == 2:
-            virus.append((i,j))
-        if arr[j] == 0:
-            empty.append((i,j))
+        if 0 < maps[i][j] < 6:
+            cctv.append((i, j, maps[i][j]))
 
-    graph.append(arr)
+def find_sight(m, directions, x, y):
+    for d in directions:
+        nx = x
+        ny = y
+        while True:
+            nx += dv[d][0]
+            ny += dv[d][1]
 
-safe = 0
-for a,b,c in combinations(empty, 3):
-    vgraph = [[0]*M for _ in range(N)]
-    for i in range(N):
-        for j in range(M):
-            vgraph[i][j] = graph[i][j]
-    for x,y in [a,b,c]:
-        vgraph[x][y] = 1
+            if nx < 0 or nx >= N or ny < 0 or ny >= M or m[nx][ny] == 6:
+                break
+            elif m[nx][ny] == 0:
+                m[nx][ny] = -1
 
-    vgraph = bfs(vgraph,virus)
-    zero = 0
-    for i in vgraph:
-        for j in i:
-            if j==0:
-                zero += 1
-    safe = max(safe, zero)
-print(safe)
+def dfs(depth, m):
+    global min_value
+    if depth == len(cctv):
+        count = 0
+        for mm in m:
+            count += mm.count(0)
+        min_value = min(min_value, count)
+        return
+    
+    tmp_maps = copy.deepcopy(m)
+    x, y, n = cctv[depth]
+
+    for directions in cctv_directions[n]:
+        find_sight(tmp_maps, directions, x, y)
+        dfs(depth+1, tmp_maps)
+        tmp_maps = copy.deepcopy(m)
+
+dfs(0, maps)
+print(min_value)
